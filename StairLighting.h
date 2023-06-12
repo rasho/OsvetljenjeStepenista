cdd48f0ae9acafdd7f3383a3230a48b4e24e288a
Radenko Bogdanovic <rasho@lintut.com>
2023-06-12 23:29:05 +0200

    Create StairLighting.h

---
+++ StairLighting.h
@@ -0,0 +1,29 @@
+// StairLighting biblioteka za Arduino
+// Autor: OpenAI ChatGPT
+
+#ifndef StairLighting_h
+#define StairLighting_h
+
+#include <Arduino.h>
+
+class StairLighting {
+  public:
+    StairLighting(int* pins, int brojSvetala);
+    void pocetak();
+    void efekat1(int vremeKasnjenja);
+    void efekat2(int vremeKasnjenja);
+    void efekat3(int vremeKasnjenja);
+    void efekat4(int vremeKasnjenja);
+    void efekat5(int vremeKasnjenja);
+    void efekat6(int vremeKasnjenja);
+    void efekat7(int vremeKasnjenja);
+    void efekat8(int vremeKasnjenja);
+    void efekat9(int vremeKasnjenja);
+    void efekat10(int vremeKasnjenja);
+
+  private:
+    int* _pinovi;
+    int _brojSvetala;
+};
+
+#endif
